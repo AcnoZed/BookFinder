ca0442ba19d382892c5baa9623c86352d1c05991
AcnoZed <Me.ouss.am@outlook.de>
2021-06-23 12:38:45 +0200

    Added Debugger

--- Start.py
+++ Start.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "<p> Hello World <p>"
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
     
