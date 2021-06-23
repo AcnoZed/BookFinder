3a2ed48c57bd9a0e2def8720d1364f77b0fb645e
AcnoZed <Me.ouss.am@outlook.de>
2021-06-23 18:41:28 +0200

    Controller saving input

--- Start.py
+++ Start.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+from models.Book import Book
+from data.BookDB import BookDB
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/home')
+@app.route('/', methods=['GET','POST'])
+@app.route('/home', methods=['GET','POST'])
 def home():
+    if request.method == 'POST':
+        req = request.form 
+        book = Book(req.get('title'), req.get('author'), req.get('genre'))
+        books = BookDB(book)
+        print(req)
+        if books is not None:
+            for book in books.get_books():
+                book.printBook()
+        
+        return redirect(request.url)
+    
+
     return render_template('home.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
