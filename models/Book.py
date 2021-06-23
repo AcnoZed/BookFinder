4cc5b7f96aeb90c0f4b08aee1b5eec6afc6e146e
AcnoZed <Me.ouss.am@outlook.de>
2021-06-23 17:31:59 +0200

    Modified BookModel

---
+++ models/Book.py
@@ -0,0 +1,15 @@
+class Book:
+
+    def __init__(self, title, author, genre=None):
+        self.title = title
+        self.author = author 
+        self.genre = genre 
+    
+    def get_title(self):
+        return self.title
+
+    def get_author(self):
+        return self.author
+
+    def get_genre(self):
+        return self.genre
