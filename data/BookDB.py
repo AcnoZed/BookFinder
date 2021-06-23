e83c26951da22a0275a9ef8b15b8368db34cf0ca
AcnoZed <Me.ouss.am@outlook.de>
2021-06-23 18:43:34 +0200

    created db

---
+++ data/BookDB.py
@@ -0,0 +1,12 @@
+class BookDB:
+
+    books = []
+    def __init__(self, book):
+        self.books.append(book)
+    
+    def add(self, book):
+        self.books.append(book)
+    
+    def get_books(self):
+        return self.books
+        
